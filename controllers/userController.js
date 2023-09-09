73197d37fb3aaeffb11de7844ba9c77903a63872
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-09-09 17:57:40 -0300

    userController preenchido

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,25 +2,24 @@ const User = require('../models/User');
 const secret = require('../config/auth.json');
 const jwt = require('jsonwebtoken');
 
-/*
 const createUser = async (req, res) => {
-    const {            } = req.body;
+    const { name, email, password} = req.body;
     await User.create({
-       
-
-
+       name:name,
+       email:email,
+       password:password
     }).then(() => {
-        res.json('         ');
-        console.log('           ');
+        res.json('Cadastro efetuado com sucesso!');
+        console.log('Cadastro efetuado com sucesso!');
     }).catch((erro) => {
-        res.json('             ');
-        console.log(`  : ${erro}`);
+        res.json('Ops! Erro no cadastro');
+        console.log(`Ops! Erro no cadastro: ${erro}`);
     })
 }
 const findUsers = async (req, res) => {
-    const     = await User.findAll();
+    const users  = await User.findAll();
     try {
-        res.json(    );
+        res.json(users);
     } catch (error) {
         res.status(404).json("Ocorreu um erro na busca!");
     };
@@ -31,22 +30,24 @@ const deleteUser = async (req, res) => {
     try {
         await User.destroy({
             where: {
-                
+                id:id
             }
         }).then(() => {
-            res.json("         ");
+            res.json("Usuario deletado com sucesso!");
         })
     } catch (error) {
-        res.status(404).json("      ");
+        res.status(404).json("ops! Erro ao deletar usuario");
     }
 }
 const updateUser = async (req, res) => {
     const id = parseInt(req.params.id);
-    const {       } = req.body;
+    const {name, email, password} = req.body;
     try {
         await User.update(
             {
-               
+               name:name,
+               email:email,
+               password:password
             },
             {
                 where: {
@@ -54,37 +55,37 @@ const updateUser = async (req, res) => {
                 }
             }
         ).then(() => {
-            res.json("            ");
+            res.json("Usuário alterado com sucesso!");
         })
     } catch (error) {
-        res.status(404).json("                !");
+        res.status(404).json("ops! Erro ao alterar usuário!");
     }
 }
 const authenticatedUser = async (req, res) => {
-    const {       } = req.body;
+    const {name, email} = req.body;
     try {
         const isUserAuthenticated = await User.findOne({
             where: {
-                
+                email:email,
+                password:password
             }
         })
         const token = jwt.sign({
-            name: 
-            email: 
+            name:name,
+            email: email
         },
             secret.secret, {
             expiresIn: 86400,
         })
         return res.json({
-            name: 
+            name: isUserAuthenticated.name,
             email: isUserAuthenticated.email,
             token: token
         });
     } catch (error) {
-        return res.json("");
+        return res.json("Usuario não encontrado");
     }
 }
 
 
 module.exports = { createUser, findUsers, deleteUser, updateUser, authenticatedUser };
-*/
