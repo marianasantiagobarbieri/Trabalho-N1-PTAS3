7e1beeb8a96fb87ffd33ad5347bdc4af89909cf1
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-09-09 18:11:20 -0300

    Update 20230816223922-users.js

--- migrations/20230816223922-users.js
+++ migrations/20230816223922-users.js
@@ -14,15 +14,15 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
       email:{
         type: Sequelize.STRING,
         allowNull:false,
         unique:true
-    } 
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
     })
   },
 
